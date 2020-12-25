c2229b65ca0e49eae7542a096cc5c25f29c61b31
AntonBrazouski <anton.brazouski@gmail.com>
2020-12-25 11:25:54 +0300

    templates

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     # Lockal
     'todos.apps.TodosConfig',
-    'users.apps.UsersConfig', # new
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
