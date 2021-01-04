eb667fb5fcf7f340c82ea4076cb4574f01365dc8
AntonBrazouski <anton.brazouski@gmail.com>
2021-01-04 11:20:24 +0300

    signup

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth', # Yoohoo !!
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL =  'home'
+LOGOUT_REDIRECT_URL = 'home' # new
