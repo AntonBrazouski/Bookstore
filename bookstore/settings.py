ad08b9a1d289b815794531744a595400b879452c
AntonBrazouski <anton.brazouski@gmail.com>
2021-01-09 11:03:22 +0300

    static files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 LOGIN_REDIRECT_URL =  'home'
 LOGOUT_REDIRECT_URL = 'home' # new
